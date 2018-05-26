f18ed225c24cf9a26f21b2e4a8817351a9442047
Xiangrong Fang <xrfang@gmail.com>
2018-05-26 23:08:17 +0800

    WIP: adding http handler

--- src/mttp/conf.go
+++ src/mttp/conf.go
@@ -1,21 +1,40 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/xrfang/go-conf"
+)
+
 type Configuration struct {
+	ALLOW_PUT     bool
+	ALLOW_WRITE   bool
+	DB_USER       string
+	DB_PASS       string
+	DB_HOST       string
+	DB_PORT       string
+	DB_NAME       string
 	HTTP_PORT     string
-	MODE          int //0=read-only; 1=write-only; 2=read-write
-	PASSWORD      string
+	READ_BATCH    int
 	READ_TIMEOUT  int
 	WRITE_TIMEOUT int
 }
 
-var cf Configuration
+var (
+	cf Configuration
+	db *sql.DB
+)
 
 func loadConfig(fn string) {
 	//default values
 	cf.HTTP_PORT = "6887"
 	cf.READ_TIMEOUT = 60
 	cf.WRITE_TIMEOUT = 60
-	if fn != "" {
-		assert(conf.ParseFile(fn, &cf))
-	}
+	err := conf.ParseFile(fn, &cf)
+	assert(err)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&sql_mode=ANSI_QUOTES",
+		cf.DB_USER, cf.DB_PASS, cf.DB_HOST, cf.DB_PORT, cf.DB_NAME)
+	db, err = sql.Open("mysql", dsn)
+	assert(err)
 }
--- src/mttp/handler.go
+++ src/mttp/handler.go
@@ -1,10 +1,65 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
+var rt *regexp.Regexp
+
+func init() {
+	rt = regexp.MustCompile(`^\w+$`)
+}
+
+func sanitizeArgs(args []string) {
+	//arguments: table_name/sort_key/sort_value
+	if !rt.MatchString(args[0]) || len(args) > 1 && !rt.MatchString(args[1]) {
+		panic(http.StatusBadRequest)
+	}
+	qry := `SELECT 1 FROM ` + args[0] + ` LIMIT 1`
+	rows, err := db.Query(qry)
+	if err != nil {
+		rows.Close()
+		panic(http.StatusNotFound)
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "TODO: handle requests\n")
+	defer func() {
+		if e := recover(); e != nil {
+			switch e.(type) {
+			case int:
+				http.Error(w, http.StatusText(e.(int)), e.(int))
+			default:
+				http.Error(w, trace(e.(error).Error()).Error(), http.StatusInternalServerError)
+			}
+		}
+	}()
+	args := strings.Split(r.URL.Path[1:], "/")
+	sanitizeArgs(args)
+	//table := args[0]
+	switch r.Method {
+	case "GET":
+		//qry := `SELECT * FROM '%s'`
+	case "HEAD":
+		//SELECT COUNT
+	case "POST":
+		if !cf.ALLOW_WRITE {
+			panic(http.StatusForbidden)
+		}
+		//INSERT IGNORE INTO
+	case "PATCH":
+		if !cf.ALLOW_WRITE {
+			panic(http.StatusForbidden)
+		}
+		//REPLACE INTO
+	case "PUT":
+		if !cf.ALLOW_WRITE || !cf.ALLOW_PUT {
+			panic(http.StatusForbidden)
+		}
+		//FLUSH + INSERT
+	default:
+		panic(http.StatusMethodNotAllowed)
+	}
 }
