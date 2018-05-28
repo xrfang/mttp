9b730a58a675451043957c891ff882f160f51fbe
xrfang <xrfang@gmail.com>
2018-05-28 11:37:00 +0800

    Added manual decryption

--- src/mttp/main.go
+++ src/mttp/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
 	ver := flag.Bool("version", false, "show version info")
 	conf := flag.String("conf", "", "configuration file")
+	dec := flag.String("decrypt", "", "decrypt given file")
 	flag.Parse()
 	if *ver {
 		fmt.Println(verinfo())
@@ -20,11 +25,25 @@ func main() {
 		return
 	}
 	loadConfig(*conf)
+	if *dec != "" {
+		var buf bytes.Buffer
+		f, err := os.Open(*dec)
+		assert(err)
+		defer f.Close()
+		_, err = io.Copy(&buf, f)
+		assert(err)
+		data := bytes.NewBuffer(Decrypt(buf.Bytes()))
+		zr, err := gzip.NewReader(data)
+		assert(err)
+		_, err = io.Copy(os.Stdout, zr)
+		assert(err)
+		return
+	}
 	http.HandleFunc("/", handler)
 	svr := http.Server{
 		Addr:         ":" + cf.HTTP_PORT,
 		ReadTimeout:  time.Duration(cf.READ_TIMEOUT) * time.Second,
 		WriteTimeout: time.Duration(cf.WRITE_TIMEOUT) * time.Second,
 	}
-	svr.ListenAndServe()
+	assert(svr.ListenAndServe())
 }
